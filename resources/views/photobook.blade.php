0aff8a426706a9ef76cbc66c638a428efafe6c30
Thomas De Meyer <demeyerthom@hotmail.com>
2017-10-28 16:22:34 +0200

    Updated photobook

--- resources/views/photobook.blade.php
+++ resources/views/photobook.blade.php
@@ -2,17 +2,23 @@
 
 @section('content')
     <div class="row bg-faded p-4 my-4">
-        <div class="row col-md-12">
-            <hr class="divider">
-            <h2 class="text-center text-lg text-uppercase my-0">Foto album</h2>
-            <hr class="divider">
-        </div>
-        <div class="row col-md-12 grid" >
+        <hr class="divider">
+        <h2 class="text-center text-lg text-uppercase">Foto album</h2>
+        <hr class="divider">
+    </div>
+    <div class="row bg-faded p-4 my-4">
+        @foreach($directories as $directory)
+            <p>
+                <a href="{{route('photobook.directory',['directory' => $directory])}}">{{basename($directory)}}</a>
+            </p>
+        @endforeach
+    </div>
+    <div class="row bg-faded p-4 my-4">
+        <div class="card-columns">
             @foreach($images as $image)
-                <div class="grid-item col-md-4">
-                    <a href="{{url($image)}}" data-toggle="lightbox"
-                       data-gallery="photobook" class="col-sm-4">
-                        <img src="{{url($image)}}" alt="..." class="img-thumbnail img-fluid">
+                <div class="card">
+                    <a href="{{url($image)}}" data-toggle="lightbox" data-gallery="photobook">
+                        <img class="card-img img-rounded" src="{{url($image)}}" alt="Card image">
                     </a>
                 </div>
             @endforeach
