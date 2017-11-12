b30e6ebbff848c01d8b4b88a2202b25dd0d239c7
Thomas De Meyer <demeyerthom@hotmail.com>
2017-11-12 12:11:12 +0100

    Changed homepage a bit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,22 +3,26 @@
 @section('content')
     <div class="bg-faded p-4 my-4">
         <div class="countdown text-center" id="js-countdown">
-            <div class="countdown-item countdown-item-large">
-                <div class="countdown-timer js-countdown-days" aria-labelledby="day-countdown"></div>
-                <div class="countdown-label" id="day-countdown">Dagen</div>
+            <div class="col-md-6">
+                <div class="countdown-item countdown-item-large">
+                    <div class="countdown-timer js-countdown-days" aria-labelledby="day-countdown"></div>
+                    <div class="countdown-label" id="day-countdown">Dagen</div>
+                </div>
             </div>
-            <div class="countdown-item">
-                <div class="countdown-timer js-countdown-hours" aria-labelledby="hour-countdown"></div>
-                <div class="countdown-label" id="hour-countdown">Uren</div>
-            </div>
-            <div class="countdown-item">
-                <div class="countdown-timer js-countdown-minutes" aria-labelledby="minute-countdown"></div>
-                <div class="countdown-label" id="minute-countdown">Minuten</div>
-            </div>
+            <div class="col-md-6">
+                <div class="countdown-item">
+                    <div class="countdown-timer js-countdown-hours" aria-labelledby="hour-countdown"></div>
+                    <div class="countdown-label" id="hour-countdown">Uren</div>
+                </div>
+                <div class="countdown-item">
+                    <div class="countdown-timer js-countdown-minutes" aria-labelledby="minute-countdown"></div>
+                    <div class="countdown-label" id="minute-countdown">Minuten</div>
+                </div>
 
-            <div class="countdown-item">
-                <div class="countdown-timer js-countdown-seconds" aria-labelledby="second-countdown"></div>
-                <div class="countdown-label" id="second-countdown">Secondes</div>
+                <div class="countdown-item">
+                    <div class="countdown-timer js-countdown-seconds" aria-labelledby="second-countdown"></div>
+                    <div class="countdown-label" id="second-countdown">Seconden</div>
+                </div>
             </div>
         </div>
     </div>
